17710269b87b1ae8873b516c3310e702dfe538a6
Maverick Chen <mavelche@gmail.com>
2013-06-18 15:50:05 -0700

    test#1

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -13,7 +13,7 @@ public class SimpleSquare
     int sides = MessageBox.askForNumericalInput("How many sides?");
     for (int i = 1; i <= sides; i++)
     {
-      Tortoise.setPenColor(Colors.getRandomColor().getHSBColor(h, s, b));
+      Tortoise.setPenColor(Colors.getRandomColor());
       Tortoise.setPenWidth(i * 3.5);
       Tortoise.move(i * 2);
       Tortoise.turn(360.0 * -8 / sides);
