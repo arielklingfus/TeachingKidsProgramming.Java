97f199ab26b903de6953aaa09bbeecf7f54ab47b
Gavin Dent <gaviindent@homtail.com>
2013-06-26 14:15:05 -0700

    AdLibs2-Gavin/Ariel

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
@@ -7,14 +7,12 @@ public class AdLibs
 {
   public static void main(String[] args)
   {
-    Words words = new words();
-    String currentAdverb = MessageBox.askForTextInput("Enter an Adverb");
-    String currentEdVerb = MessageBox.askForTextInput("Enter a verb ending in -ed");
-    String currentBodyPart = MessageBox.askForTextInput("Enter a Body Part");
-    String currentStory = Parser.parse("Today I woke[currentAdverb]", null);
-    currentStory += " I woke " + currentAdverb + ".";
-    currentStory += " Then I " + currentEdVerb + " ";
-    currentStory += " my " + currentBodyPart + ".";
+    Words words = new Words();
+    words.currentAdverb = MessageBox.askForTextInput("Enter an Adverb");
+    words.currentEdVerb = MessageBox.askForTextInput("Enter a verb ending in -ed");
+    words.currentBodyPart = MessageBox.askForTextInput("Enter a Body Part");
+    String currentStory = Parser.parse(
+        "Today I woke {currentAdverb}. Then I {currentEdVerb} my {currentBodyPart}", words);
     MessageBox.showMessage(currentStory);
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Words.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Words.java
@@ -2,4 +2,7 @@ package org.teachingkidsprogramming.recipes;
 
 public class Words
 {
+  public String currentAdverb;
+  public String currentEdVerb;
+  public String currentBodyPart;
 }
